e5b9c99924f600c76d35d774f8aa080654c06138
Iusto <iusto@naver.com>
2022-10-24 20:26:07 +0900

    first commit

--- 중복된 문자 제거.py
+++ 중복된 문자 제거.py
@@ -1,5 +1,4 @@
 def solution(my_string):
-    my_string = list(my_string)
-    my_string[:] = list(dict.fromkeys(my_string))
+    my_string = list(dict.fromkeys(my_string))
     answer = "".join(my_string)
     return answer
