edbd8fe1a4ce4d8222deaa50df34a393b240657c
Iusto <iusto@naver.com>
2023-04-07 18:04:20 +0900

    second commit

--- lv2/[3차] 파일명 정렬.py
+++ lv2/[3차] 파일명 정렬.py
@@ -1,8 +1,20 @@
-import re
-
 def solution(files):
-    a = sorted(files, key=lambda x : int(re.findall('\d{1,5}', x)[0])) 
-    # 한 글자에서 최대 다섯 글자 사이 짝대기 뒤에 숫자패턴으로 정렬
-    b = sorted(a, key=lambda x : re.split('\d{1,5}', x.lower())[0])
-    # 짝대기 앞에 알파벳 패턴 분리 후 정렬, 문자열 비교 대소문자 구분 안함
-    return b
+    answer = []
+    for f in files:
+        head, number, tail = '', '', ''
+
+        number_check = False
+        for i in range(len(f)): # 문자열 자르기
+            if f[i].isdigit():  # 처음 나오는 숫자부터는 NUMBER로
+                number += f[i]
+                number_check = True
+            elif not number_check:  # NUMBER가 나오기 전까지는 HEAD
+                head += f[i]
+            else:               # NUMBER가 이미 나왔고, 숫자가 아닌 문자가 나오면 TAIL
+                tail = f[i:]
+                break
+        answer.append((head, number, tail))  # HEAD, NUMBER, TAIL 하나의 튜플로 저장
+
+    answer.sort(key=lambda x: (x[0].upper(), int(x[1])))  # HEAD 우선, NUMBER 차선으로 정렬
+
+    return [''.join(t) for t in answer]   # 원래 형태로 문자열 만들어서 반환
